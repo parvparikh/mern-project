5b16a15cc8cddcf83dcfecef348ff0174cc30aad
Parv Parikh <parvparikh11@gmail.com>
2024-06-06 16:42:38 +0530

    Create User Model

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
