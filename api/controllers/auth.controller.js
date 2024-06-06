49c768cb4363e25d347f0a81e1307fde999532fc
Parv Parikh <parvparikh11@gmail.com>
2024-06-06 18:39:41 +0530

    Create Error Handler and a Middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,12 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
-export const signup = async (req,res) => {
+import { errorHandler } from "../utils/error.js";
+export const signup = async (req,res,next) => {
     const {username,email,password} = req.body;
 
     if(!username || !email || !password || username===' ' || email===' ' || password===' ')
         {
-            return res.status(400).json({ message: 'ALL REQUIRED'});
+            next(errorHandler(400,"ALL REQUIRED!!!"));
         }
 
         const hashedPassword = bcryptjs.hashSync(password,10);
@@ -19,6 +20,6 @@ export const signup = async (req,res) => {
         await newUser.save();
         res.json({message: 'SIGNUP Success!!'})
         } catch (error) {
-            res.status(500).json({message: error.message})
+            next(error);
         }
 };
