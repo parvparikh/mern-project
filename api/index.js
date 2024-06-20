bf9760a28a97b8df325db60d8b7783a4397a9571
Parv Parikh <parvparikh11@gmail.com>
2024-06-20 17:30:58 +0530

    add update user api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 //import cors from 'cors';
 
 console.log('its definately taking');
@@ -13,7 +14,7 @@ mongoose.connect(process.env.MONGO).then(
 console.log(' or here 13 time here right');
 
 const app = express();
-
+app.use(cookieParser());
 /*app.use(cors({
     origin: 'http://localhost:5173', // Replace with your frontend origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
